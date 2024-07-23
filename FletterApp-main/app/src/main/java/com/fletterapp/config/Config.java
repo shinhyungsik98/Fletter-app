95e8e5ea67debf2b8061c0825bafd36086d06531
shinhyungsik98 <ragi08@naver.com>
2024-07-23 12:03:16 +0900

    first upload

---
+++ FletterApp-main/app/src/main/java/com/fletterapp/config/Config.java
@@ -0,0 +1,6 @@
+package com.choiminseon.fletterapp.config;
+
+public class Config {
+    public static final String DOMAIN = "https://r27s19ugb5.execute-api.ap-northeast-2.amazonaws.com";
+    public static final String SP_NAME = "flatter_app";
+}
